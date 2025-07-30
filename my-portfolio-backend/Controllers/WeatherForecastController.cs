5245431e5357bb07c393d91a1c749bd17924b500
luongba <a2k10lnc@gmail.com>
2025-07-31 00:11:24 +0700

    test build be

--- my-portfolio-backend/Controllers/WeatherForecastController.cs
+++ my-portfolio-backend/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace my_portfolio_backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Coolee", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
